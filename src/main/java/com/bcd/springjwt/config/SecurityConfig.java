a6b03c3817821337d497baf18067486e6797b63f
CJC0512 <ehdchs2006@gmail.com>
2024-07-16 11:13:52 +0900

    login

--- src/main/java/com/bcd/springjwt/config/SecurityConfig.java
+++ src/main/java/com/bcd/springjwt/config/SecurityConfig.java
@@ -1,16 +1,36 @@
 package com.bcd.springjwt.config;
 
+import com.bcd.springjwt.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
 
     // 시큐리티를 통해서 회원정보를 활용할 경우, 항상 비밀번호를 캐쉬로 암호화하고 진행해야하기에, 이를 암호화하는 BCryptPasswordEncoder를 활용한다.
     @Bean
@@ -40,6 +60,9 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll()        // 모두 허용되는 경로
                         .requestMatchers("/admin").hasRole("ADMIN")                     // admin 경로는 ADMIN 권한을 가진 사용자만 접근 가능
                         .anyRequest().authenticated());                                   // 다른 모든 경로에는 로그인한 사용자만 접근 가능
+
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         // 세션 설정 (state less 상태로 관리)
         http
